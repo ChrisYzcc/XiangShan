a317f61dfc38f6876037d16c89c99523b3b26545
yangzechen <yangzechen21@mails.ucas.ac.cn>
2025-01-01 12:49:16 +0800

    chore: support benchmark selection

--- scripts/tester.mk
+++ scripts/tester.mk
@@ -10,19 +10,28 @@ $(EMU):
 
 gen: $(EMU)
 
-WORKLOAD = ./ready-to-run/microbench.bin
+WORKLOAD？= microbench
+WORKLOAD_PATH？=
 DIFF_SO = ./ready-to-run/riscv64-nemu-interpreter-so
 
 SIM_OUT = ./rpt/simulation_out.txt
 SIM_ERR = ./rpt/simulation_err.txt
 
+ifeq ($(WORKLOAD), microbench)
+	WORKLOAD_PATH = ./ready-to-run/microbench.bin
+else ifeq ($(WORKLOAD), coremark)
+	WORKLOAD_PATH = /nfs/home/share/ci-workloads/nexus-am-workloads/apps/coremark/coremark-riscv64-xs.bin
+else
+	echo "Unknown workload."
+	exit
+endif
+
 run: $(EMU)
 	-mkdir rpt
-	$(EMU) -i $(WORKLOAD) --diff $(DIFF_SO) 2>$(SIM_ERR) | tee $(SIM_OUT)
+	$(EMU) -i $(WORKLOAD_PATH) --diff $(DIFF_SO) 2>$(SIM_ERR) | tee $(SIM_OUT)
 
 $(SIM_OUT): run
 $(SIM_ERR): run
-
 
 # Performance
 PERF_CSV = ./rpt/stats.csv
