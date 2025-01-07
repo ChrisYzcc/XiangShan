684a95c65fdd51728beabe548fad663a479b34c9
yangzechen <yangzechen21@mails.ucas.ac.cn>
2025-01-07 14:24:59 +0800

    feat: add script for analyzing chiselDB result

--- scripts/mshr_analysis.py
+++ scripts/mshr_analysis.py
@@ -1,41 +1,56 @@
 # Analysing the MSHR occupancy of the DCache
 
-import re
+import sqlite3
 
-pattern = r"Cycle (\d+): Unalloc: (\d+), Prefetch: (\d+), Mixed: (\d+), Other: (\d+)"
+class MSHRData:
+    MSHR_UNALLOC    = 0
+    MSHR_PREFETCH   = 1
+    MSHR_MIXED      = 2
+    MSHR_OTHER      = 3
 
-res = []
-# Read the file
-with open("./rpt/dcache_mshr.log", "r") as file:
-    for line in file:
-        match = re.match(pattern, line)
-        if match:
-            cycle, unalloc, prefetch, mixed, other = map(int, match.groups())
-            res.append((cycle, unalloc, prefetch, mixed, other))
+    def __init__(self):
+        self.unalloc = 0
+        self.prefetch = 0
+        self.mixed = 0
+        self.other = 0
 
-# Calculate the occupation rates
-index = []
-unalloc_rates = []
-prefetch_rates = []
-mixed_rates = []
-other_rates = []
-for entry in res:
-    cycle, unalloc, prefetch, mixed, other = entry
-    total = unalloc + prefetch + mixed + other
-    index.append(cycle)
-    unalloc_rates.append(unalloc / total)
-    prefetch_rates.append(prefetch / total)
-    mixed_rates.append(mixed / total)
-    other_rates.append(other / total)
+    def __inc__(self, state):
+        if state == MSHRData.MSHR_UNALLOC:
+            self.unalloc += 1
+        elif state == MSHRData.MSHR_PREFETCH:
+            self.prefetch += 1
+        elif state == MSHRData.MSHR_MIXED:
+            self.mixed += 1
+        elif state == MSHRData.MSHR_OTHER:
+            self.other += 1
 
-# Plot the results
-import matplotlib.pyplot as plt
-plt.plot(index, unalloc_rates, label="Unallocated", color="blue")
-plt.plot(index, prefetch_rates, label="Prefetch", color="green")
-plt.plot(index, mixed_rates, label="Mixed", color="red")
-plt.plot(index, other_rates, label="Other", color="black")
-plt.xlabel("Cycle")
-plt.ylabel("Occupancy rate")
-plt.legend()
-plt.show()
-plt.savefig("./rpt/dcache_mshr.png")
+    def __str__(self):
+        return "Unalloc: " + str(self.unalloc) + ", Prefetch: " + str(self.prefetch) + ", Mixed: " + str(self.mixed) + ", Other: " + str(self.other)
+
+db_path_list = ["./build/2025-01-07@13:52:41.db"]
+
+# Process each database
+for path in db_path_list:
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+
+    mshr_datas = {}
+    # Get the data
+    for i in range(0, 15):
+        raw_data = c.execute("SELECT * FROM " + "mshrStateTable" + str(i) + ";").fetchall()
+
+        for data in raw_data:
+            timestamp = data[2]
+
+            if timestamp not in mshr_datas:
+                mshr_datas[timestamp] = MSHRData()
+            
+            mshr_datas[timestamp].__inc__(data[1])
+
+    for key in mshr_datas:
+        print("Timestamp: " + str(key) + ", " + mshr_datas[key].__str__())
+
+    # Close the connection
+    c.close()
+    conn.close()
+
