b0f4883e40a964b198dd74c4d2d322546e9d8a78
yzcc <yangzechen21@mails.ucas.ac.cn>
2025-01-14 16:02:55 +0800

    feat: enable constantin

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -569,7 +569,7 @@ case class DebugOptions
   EnableDebug: Boolean = false,
   EnablePerfDebug: Boolean = true,
   UseDRAMSim: Boolean = false,
-  EnableConstantin: Boolean = false,
+  EnableConstantin: Boolean = true,
   EnableChiselDB: Boolean = true,
   AlwaysBasicDB: Boolean = true,
   EnableRollingDB: Boolean = false
