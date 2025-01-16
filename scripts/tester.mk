df89585c8efbb38dad45dcb9c2d2e7945dfe2f05
yangzechen <yangzechen21@mails.ucas.ac.cn>
2025-01-16 11:37:33 +0800

    update script

--- scripts/tester.mk
+++ scripts/tester.mk
@@ -44,10 +44,11 @@ PERF_PATH = ~/env-scripts/perf
 GCPT_PATH = /nfs/home/share/liyanqin/spec06_rv64gcb_O3_20m_gcc12.2.0-intFpcOff-jeMalloc/checkpoint-0-0-0
 #JSON_PATH = $(NOOP_HOME)/scripts/simpoint_summary.json
 JSON_PATH = /nfs/home/share/liyanqin/env-scripts/perf/json/gcc12o3-incFpcOff-jeMalloc-0.3.json
-SERVER_LIST = open06 open07 open08 open09 open10 open12 open13 open14 open15
+#SERVER_LIST = open06 open07 open08 open09 open10 open12 open13 open14 open15
+SERVER_LIST = node005 node006 node007 node008 node009 node027 node028
 XS_PATH = $(NOOP_HOME)
 
-SIMPOPINT_RESDIR = ~/offchip-with-prefetch
+SIMPOPINT_RESDIR = ~/offchip-access/with-prefetch
 simpoint:
 	-@rm -rf $(SIMPOPINT_RESDIR)
 	$(PYTHON) $(PERF_PATH)/xs_autorun_multiServer.py $(GCPT_PATH) $(JSON_PATH) --xs $(XS_PATH) --threads 16 --dir $(SIMPOPINT_RESDIR) --resume -L "$(SERVER_LIST)"
